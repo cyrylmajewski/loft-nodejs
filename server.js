6b9ac8777a7082accb829aa53686656fabcd02a0
Cyryl Majewski <c.majewski@twoheads.pl>
2021-10-17 10:31:37 +0200

    fix(connections): deleted connections

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const LIMIT = 10;
 const DELAY = 1000;
 const PORT = 3000;
 
-let connections = [];
-
 let timer = function(req, res, next) {
   let tick = 0;
 
@@ -32,8 +30,6 @@ app.use(timer);
 app.get('/date', (req, res, next) => {
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.setHeader('Transfer-Encoding', 'chunked');
-
-  connections.push(res);
 });
 
 app.listen(PORT, () => {
